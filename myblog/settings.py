50475d24715a4eb5afdaae8f56c76f4ddf03cd9c
deng-2017 <953970122@qq.com>
2018-06-21 13:10:00 +0800

    首次部署

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!sbq#14qfvf=6+0akp=y)q)3t=l5_&c36_%grho1+aesz(83k4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.dcookies.xyz']
 
