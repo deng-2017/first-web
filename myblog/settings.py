cf8ceb0ca13fd7562ba6aecd7d95b1ee2b632198
deng-2017 <953970122@qq.com>
2018-06-27 22:56:55 +0800

    Add files via upload

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!sbq#14qfvf=6+0akp=y)q)3t=l5_&c36_%grho1+aesz(83k4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.dcookies.xyz']
 
 
 # Application definition
